549cf150ad9507563eccb9564cd4063ca24c106e
Luke Yianni <lukey@aleios.com>
2023-04-28 13:46:41 +0100

    refactor: remove s3

--- lib/test_project-stack.ts
+++ lib/test_project-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
-import { BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
-import { AleiosBucket } from '../constructs/S3Bucket';
 import { HelloWorld } from '../functions';
 
 export class TestProjectStack extends cdk.Stack {
@@ -10,9 +8,5 @@ export class TestProjectStack extends cdk.Stack {
     super(scope, id, props);
 
     new HelloWorld(this, 'HelloWorldFunction');
-
-    new AleiosBucket(this, 'testBucket', {
-      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
-    });
   }
 }
